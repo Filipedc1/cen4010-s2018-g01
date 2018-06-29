272ef35b494c610f357b08827431d9cf67a898e9
fcatarcione91 <fcatarcione91@gmail.com>
2018-06-28 21:31:42 -0400

    Removed unused DbSet from dbcontext class

--- CampusSnapshots/SnapshotsData/SnapshotsDbContext.cs
+++ CampusSnapshots/SnapshotsData/SnapshotsDbContext.cs
@@ -14,6 +14,5 @@ namespace SnapshotsData
         //this represents a table in the database
         public DbSet<Member> Members { get; set; }
         public DbSet<Post> Posts { get; set; }
-        public DbSet<Status> Status { get; set; } //NEW dont apply migrations since table already exists
     }
 }
